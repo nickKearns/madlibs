f77088ec0f2e9c79f8103e0a0d2c6a23b709c168
nickKearns <nickkearns133@gmail.com>
2019-08-30 16:50:14 -0700

    [add] functions and function calls

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,39 @@
+
+noun1 = ""
+verb1 = ""
+noun2 = ""
+verb2 = ""
+noun3 = ""
+verb3 = ""
+adjective1 = ""
+noun4 = ""
+noun5 = ""
+
+def get_a_noun():
+    print("Please enter a noun.")
+    given_noun = str(input())
+    return given_noun 
+
+def get_a_verb():
+        print("Please enter a verb.")
+        given_verb = str(input())
+        return given_verb
+
+def get_an_adjective():
+    print("Please enter an adjective.")
+    given_adjective = str(input())
+    return given_adjective
+
+def get_verb_ing():
+    print("Please enter a verb ending in ing.")
+    given_verb_ing = str(input())
+    return given_verb_ing
+
+
+
+
 # DISPLAY THE STORY WITH BLANKS
+print("Stairway to Heaven Madlib")
 print('''There's a (NOUN)) who's sure
 All that (VERB) is (NOUN)
 And she's (VERB ending in ING) a stairway to (NOUN)
@@ -6,3 +41,36 @@ When she gets there she (VERB)
 If the stores are all (ADJECTIVE)
 With a (NOUN) she can get what she came for
 Oh oh oh oh and she's buying a stairway to (NOUN)''')
+
+
+
+
+#COLLECT INPUTS
+noun1 = get_a_noun()
+verb1 = get_a_verb()
+noun2 = get_a_noun()
+verb2 = get_verb_ing()
+noun3 = get_a_noun()
+verb3 = get_a_verb()
+adjective1 = get_an_adjective()
+noun4 = get_a_noun()
+noun5 = get_a_noun()
+
+
+#print("Please enter a verb.")
+
+#print("Please enter a noun.")
+
+#print("Please enter a verb ending with ing.")
+
+#print("Please enter a noun.")
+
+#print("Please enter an adjective.")
+
+#print("Please enter a noun.")
+
+#print("Please enter a noun.")
+
+
+#FUNCTIONS TO GET EACH TYPE OF SPEECH
+
